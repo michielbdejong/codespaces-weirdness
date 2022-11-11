3b24abc5201fc715bbd52854b5660035047ee57a
Michiel de Jong <michielbdejong@users.noreply.github.com>
2022-11-11 15:27:58 +0000

    Did the oc2 image get built with the correct env var for HOST?

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CodeSpaces Weirdness
 
+Super weird:
+Open this repo in GitHub Codespaces, then:
+```
+./build.sh
+docker run -it oc2 env | grep HOST=
+# you will see HOST=oc1
+touch servers/oc2/Dockerfile
+docker run -it oc2 env | grep HOST=
+# you will see HOST=oc2
+```
--- test.sh
+++ test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-# base image for owncloud image:
+echo building base image
 cd servers/owncloud
 docker build -t owncloud .
 
-# image for oc1:
+echo building oc1
 cd ../oc1
-docker build -t oc1 .
+docker build --no-cache -t oc1 .
 
-# image for oc2:
+echo building oc2
 cd ../oc2
-docker build -t oc2 .
+docker build --no-cache -t oc2 .
+
+echo Did the oc2 image get built with the correct env var for HOST?
+docker run -it oc2 env | grep HOST=
